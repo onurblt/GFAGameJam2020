c087f26411ffc8088335d7f5be41fe28758a6fcd
Atakan Senturk <blue.atakan.14@gmail.com>
2020-12-18 18:48:00 +0300

    last changes

---
+++ Assets/Scripts/ShootController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    //Kartopu Objesi
+    public GameObject karTopu;
+
+    public float fırlatmaHızı = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Rigidbody karTopuRigidbody = karTopu.GetComponent<Rigidbody>();
+            karTopuRigidbody.useGravity = true;
+            karTopuRigidbody.AddForce(transform.forward * fırlatmaHızı, ForceMode.Impulse);
+        }
+    }
+}
