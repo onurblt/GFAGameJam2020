ad75360e2a8576f0a5f9fe0e122abe0daf2b0f22
Onur Bulut <onurbulut@protonmail.com>
2020-12-18 18:57:59 +0300

    Character movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    private CharacterController controller;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = Vector3.zero;
+        if(Input.GetKey(KeyCode.A))
+        {
+            dir.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dir.x = 1;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            dir.z = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            dir.z = -1;
+        }
+
+        controller.Move(dir*speed * Time.deltaTime);
+    }
+}
