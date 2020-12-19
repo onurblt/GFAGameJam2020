bf47dacf78b8b2e0da9c2f471d3f3183f0d6b9f9
Atakan Senturk <blue.atakan.14@gmail.com>
2020-12-20 01:31:03 +0300

    UI Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject startPanel;
-    public GameObject healthBar;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartGame()
     {
-        Debug.Log("asdas");
-        startPanel.SetActive(false);
-        healthBar.SetActive(true);
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Additive);
     }
 }
